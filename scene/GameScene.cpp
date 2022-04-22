305d648d038d173f82d75ef55abdf69d85b28558
NishizawaYuga <n.sutamare.2002@outlook.jp>
2022-04-22 12:02:08 +0900

    02_01_ex3課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -22,38 +22,39 @@ void GameScene::Initialize() {
 	model_ = Model::Create();
 
 	//並べる用の変数
-	float positionX = -16.0f;
-	float positionY = -16.0f;
-	float shift = 4.0f;
+	float positionX = -12.0f;
+	float positionY = -12.0f;
+	float positionZ = 0.0f;
+	float shift = 3.0f;
 
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-			if (i * j % 2 == 1) {
+			for (int k = 0; k < 9; k++) {
 				// X , Y , Z 方向のスケーリングを設定
-				//iとjをかけた数が奇数なら表示しない
-				worldTransform_[i][j].scale_ = {0.0f, 0.0f, 0.0f};
-			} else {
-				//それ以外は表示させる
-				worldTransform_[i][j].scale_ = {1.0f, 1.0f, 1.0f};
+				worldTransform_[i][j][k].scale_ = {1.0f, 1.0f, 1.0f};
+
+				// X , Y , Z 軸周りの回転角を設定
+				//※度数法での指定
+				worldTransform_[i][j][k].rotation_ = {0.0f, 0.0f, 0.0f};
+
+				// X , Y , Z 軸周りの平行移動を指定
+				worldTransform_[i][j][k].translation_ = {positionX, positionY, positionZ};
+				// 3Dモデルの位置をずらしていく
+				positionX += shift;
 			}
-
-			// X , Y , Z 軸周りの回転角を設定
-			//※度数法での指定
-			worldTransform_[i][j].rotation_ = {0.0f, 0.0f, 0.0f};
-
-			// X , Y , Z 軸周りの平行移動を指定
-			worldTransform_[i][j].translation_ = {positionX, positionY, 0.0f};
-			// 3Dモデルの位置をずらしていく
-			positionX += shift;
+			positionY += shift;
+			positionX = -12.0f;
 		}
-		positionY += shift;
-		positionX = -16.0f;
+		positionZ += shift * 1.4f;
+		positionY = -12.0f;
 	}
 
 	//ワールドトランスフォームの初期化
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-			worldTransform_[i][j].Initialize();
+			for (int k = 0; k < 9; k++) {
+				worldTransform_[i][j][k].Initialize();
+			}
 		}
 	}
 
@@ -102,7 +103,9 @@ void GameScene::Draw() {
 	// 3Dモデル描写
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-			model_->Draw(worldTransform_[i][j], viewProjection_, textureHandle_);
+			for (int k = 0; k < 9; k++) {
+				model_->Draw(worldTransform_[i][j][k], viewProjection_, textureHandle_);
+			}
 		}
 	}
 

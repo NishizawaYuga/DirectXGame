ffc72991e4d2b46856f4faffa13e380c2eac484d
NishizawaYuga <n.sutamare.2002@outlook.jp>
2022-04-22 11:38:06 +0900

    02_01_ex2課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -22,33 +22,39 @@ void GameScene::Initialize() {
 	model_ = Model::Create();
 
 	//並べる用の変数
-	float positionX = -40.0f;
+	float positionX = -16.0f;
+	float positionY = -16.0f;
+	float shift = 4.0f;
 
-	for (int i = 0; i < 18; i++) {
-		// X , Y , Z 方向のスケーリングを設定
-		worldTransform_[i].scale_ = {5.0f, 5.0f, 5.0f};
-
-		// X , Y , Z 軸周りの回転角を設定
-		//※度数法での指定
-		worldTransform_[i].rotation_ = {0.0f, 0.0f, 0.0f};
-
-		if (i < 9) {
-			// X , Y , Z 軸周りの平行移動を指定
-			worldTransform_[i].translation_ = {positionX, -20.0f, 0.0f};
-			// 3Dモデルの位置をずらしていく
-			positionX += 9.9f;
-		} else {
-			if (i == 9) {
-				positionX = -40;
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (i * j % 2 == 1) {
+				// X , Y , Z 方向のスケーリングを設定
+				//iとjをかけた数が奇数なら表示しない
+				worldTransform_[i][j].scale_ = {0.0f, 0.0f, 0.0f};
+			} else {
+				//それ以外は表示させる
+				worldTransform_[i][j].scale_ = {1.0f, 1.0f, 1.0f};
 			}
-			worldTransform_[i].translation_ = {positionX, 20.0f, 0.0f};
-			positionX += 9.9f;
+
+			// X , Y , Z 軸周りの回転角を設定
+			//※度数法での指定
+			worldTransform_[i][j].rotation_ = {0.0f, 0.0f, 0.0f};
+
+			// X , Y , Z 軸周りの平行移動を指定
+			worldTransform_[i][j].translation_ = {positionX, positionY, 0.0f};
+			// 3Dモデルの位置をずらしていく
+			positionX += shift;
 		}
+		positionY += shift;
+		positionX = -16.0f;
 	}
 
 	//ワールドトランスフォームの初期化
-	for (int i = 0; i < 18; i++) {
-		worldTransform_[i].Initialize();
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			worldTransform_[i][j].Initialize();
+		}
 	}
 
 	//ビュープロジェクションの初期化
@@ -94,8 +100,10 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 	// 3Dモデル描写
-	for (int i = 0; i < 18; i++) {
-		model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			model_->Draw(worldTransform_[i][j], viewProjection_, textureHandle_);
+		}
 	}
 
 	// 3Dオブジェクト描画後処理
